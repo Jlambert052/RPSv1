b6772b492ab7c9b8806281a8a09b12f2d2db710c
Jack Lambert <jackwlambert14@gmail.com>
2022-10-10 15:43:57 -0400

    v1.0 RPS needs replay fix

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Welcome to the RPS world championships; it's you vs the ultimate RPS robot from planet Zorgon. \nIf you lose, all of humanity gets crushed by a big rock. Good luck.");
+
+static void Main(string[] args) {
+}
+
+var instance = new RPSgame.RPS();
+
+instance.RpsPlay(); 
---
+++ RPS.cs
@@ -0,0 +1,85 @@
+namespace RPSgame;
+
+    public class RPS {
+
+        Random randomNum = new();
+
+        public int RpsPlay() {
+            string inputPlayer;
+            string Computer;
+            int randomMove;
+            bool playAgain = true;
+            string[] RPSoptions = new string[3] {"ROCK", "PAPER", "SCISSORS" };
+            int wins = 0;
+            int losses = 0;
+            int ties = 0;
+            bool replay = false;
+            while(playAgain){
+
+            randomMove = randomNum.Next(1, 4);
+            Console.WriteLine("To Play enter ROCK, PAPER, or SCISSORS!");
+            inputPlayer = Console.ReadLine().ToUpper();
+            if(!RPSoptions.Contains(inputPlayer)) {
+                System.Console.WriteLine($"{inputPlayer} is not a valid move; please enter ROCK, PAPER, or SCISSORS.");
+                inputPlayer = Console.ReadLine().ToUpper();
+            }
+            
+            switch(randomMove) {
+
+                case 1:
+                    Computer = "ROCK";
+                    if(inputPlayer == "ROCK") {
+                        System.Console.WriteLine("It's a tie!");
+                        ties ++;
+                    } else if(inputPlayer == "PAPER") {
+                        System.Console.WriteLine("Paper beats rock! You got them!");
+                        wins ++;
+                    } else if(inputPlayer == "SCISSORS") {
+                        System.Console.WriteLine("Your Scissors are crushed; Ouch!");
+                        losses ++;
+                    }
+                    break;
+                case 2:
+                    Computer = "PAPER";
+                    if(inputPlayer == "ROCK") {
+                        System.Console.WriteLine("Ouch, your rock gets wrapped up.");
+                        losses ++;
+                    } else if(inputPlayer == "PAPER") {
+                        System.Console.WriteLine("It's a tie!");
+                        ties ++;
+                    } else if(inputPlayer == "SCISSORS") {
+                        System.Console.WriteLine("Snip snip! You win!");
+                        wins ++;
+                    }
+
+                    break;
+                case 3:
+                    Computer = "SCISSORS";
+                    if(inputPlayer == "ROCK") {
+                        System.Console.WriteLine("You crush their scissors!");
+                        wins ++;
+                    } else if(inputPlayer == "PAPER") {
+                        System.Console.WriteLine("Oh no! You get diced up.");
+                        losses ++;
+                    } else if(inputPlayer == "SCISSORS") {
+                        System.Console.WriteLine("You can snip scissors; tie!");
+                        ties ++;
+                    }
+                    break;
+
+            }
+            System.Console.WriteLine($"You have {wins} wins, {losses} losses, and {ties} ties.");
+            if(wins > losses) {
+                System.Console.WriteLine("Great job; the robot is losing and earth is safe");
+            } else if (wins < losses) {
+                System.Console.WriteLine("Bad news; the robot is winning; not looking good for earth. Maybe we should sub someone else in?");
+            } else if(wins == losses) {
+                System.Console.WriteLine("Close match; this next round could decide it all!");
+            }
+            //find way to loop play again.
+
+            }
+            System.Console.WriteLine("Thank you for playing!");
+            return wins;
+        }
+    }
